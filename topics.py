abc154a4820e232e8dd3004d43d870dfe15076e4
AmandaHLC <001719cat58hay@gmail.com>
2025-07-14 19:40:55 +0800

    Add files via upload

---
+++ topics.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Topics.ipynb
+設計一個函數，輸入為數字，輸出則為*符號為輸入數字邊長的空心正三角形。
+輸入: 3
+輸出：
+    *
+   * *
+  * * *  
+"""
+
+def print_hollow_triangle(size):
+
+  if size <= 0:
+    print("邊長必須是正整數。")
+    return
+
+  # Top row
+  print(" " * (size - 1) + "*")
+
+  # Middle rows
+  for i in range(2, size):
+    spaces_before = size - i
+    spaces_inside = (i - 2) * 2 + 1
+    print(" " * spaces_before + "*" + " " * spaces_inside + "*")
+
+  # Bottom row
+  if size > 1:
+    print("* " * (size - 1) + "*")
+
+#print_hollow_triangle(3)
+
+size = int(input("Triangle size? "))
+print_hollow_triangle(size)
